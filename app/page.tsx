8054580c2c6325d271f302144ceb01ccf3085583
Peng-Yu Chen <me@pengyuc.com>
2024-02-05 23:52:55 -0500

    feat: add button in the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
+import { Poppins } from 'next/font/google';
+
+import { LoginButton } from '@/components/auth/login-button';
 import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
+const font = Poppins({
+  subsets: ['latin'],
+  weight: ['300'],
+});
 
 export default function Home() {
   return (
-    <div>
-      <Button size="lg">Click me</Button>
-    </div>
+    <main className="flex h-full flex-col items-center justify-center bg-gradient-to-tr from-sky-200 to-sky-600">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            'text-6xl font-semibold text-white drop-shadow-md',
+            font.className,
+          )}
+        >
+          Next.js Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication service</p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign In
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
