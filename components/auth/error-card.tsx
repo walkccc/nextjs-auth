a5f995e422cf910bf9a6888ab31990db5cd295f5
Peng-Yu Chen <me@pengyuc.com>
2024-02-07 23:04:53 -0500

    feat: add Google and GitHub login
    - add auth error page
    - handle `OAuthAccountNotLinked`

---
+++ components/auth/error-card.tsx
@@ -0,0 +1,17 @@
+import { ExclamationTriangleIcon } from '@radix-ui/react-icons';
+
+import { CardWrapper } from '@/components/auth/card-wrapper';
+
+export const ErrorCard = () => {
+  return (
+    <CardWrapper
+      headerLabel="Oops! Something went wrong!"
+      backButtonLabel="Back to login"
+      backButtonHref="/auth/login"
+    >
+      <div className="w-full flex justify-center items-center">
+        <ExclamationTriangleIcon className="text-destructive w-8 h-8" />
+      </div>
+    </CardWrapper>
+  );
+};
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useSearchParams } from 'next/navigation';
 import { useState, useTransition } from 'react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
@@ -22,6 +23,13 @@ import { Input } from '@/components/ui/input';
 import { LoginSchema } from '@/schemas';
 
 export const LoginForm = () => {
+  const searchParams = useSearchParams();
+
+  const urlError =
+    searchParams.get('error') === 'OAuthAccountNotLinked'
+      ? 'Email in use with a different provider!'
+      : '';
+
   const [error, setError] = useState<string | undefined>('');
   const [success, setSuccess] = useState<string | undefined>('');
   const [isPending, startTransition] = useTransition();
@@ -92,7 +100,7 @@ export const LoginForm = () => {
               )}
             />
           </div>
-          <FormError message={error} />
+          <FormError message={error || urlError} />
           <FormSuccess message={success} />
           <Button type="submit" disabled={isPending} className="w-full">
             Login
