f67e68aa163319c8831f2bd061687075b2caa9f2
Peng-Yu Chen <me@pengyuc.com>
2024-02-15 14:43:07 -0500

    feat: add `id` and `role` in session by `callbacks`
    $ npx prisma generate
    $ npx prisma migrate reset
    $ npx prisma db push

--- auth.ts
+++ auth.ts
@@ -1,7 +1,9 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import { UserRole } from '@prisma/client';
 import NextAuth from 'next-auth';
 
 import authConfig from '@/auth.config';
+import { getUserById } from '@/data/user';
 import { db } from '@/lib/db';
 
 export const {
@@ -10,6 +12,25 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user && token.sub) {
+        session.user.id = token.sub;
+      }
+      if (session.user && token.role) {
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+
+      return { ...token, role: existingUser.role };
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,
