35d8c22cbbf6446c16ebc9c3046ae911c38e6861
Peng-Yu Chen <me@pengyuc.com>
2024-02-16 09:42:07 -0500

    fix: `return` nothing for `auth` in `middleware.ts`

--- middleware.ts
+++ middleware.ts
@@ -19,14 +19,14 @@ export default auth((req) => {
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
 
   if (isApiAuthRoute) {
-    return null;
+    return;
   }
 
   if (isAuthRoute) {
     if (isLoggedIn) {
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
-    return null;
+    return;
   }
 
   if (!isLoggedIn && !isPublicRoute) {
@@ -42,7 +42,7 @@ export default auth((req) => {
     );
   }
 
-  return null;
+  return;
 });
 
 // Optionally, don't invoke Middleware on some paths
